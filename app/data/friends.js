d1466122aa47101140ce7c14d8f83be5cf2f7b51
RalphWiley <nazkeith@gmail.com>
2018-04-17 19:56:38 -0500

    started basics

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+
+//array that holds all the friends that have taken the survey
+var friendList = [
+    {
+     name : "Ralph Wiley",
+     profilePic: "image",
+     scores:[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+        ]
+    }
+]
+
+module.exports = friendList;
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,15 @@
+// Dependencies
+var path = require("path");
+
+//Routing
+
+module.exports = function(app){
+//route to survey page
+  app.get("/survey", function(req, res) {
+      res.sendFile(path.join(__dirname, "/../public/survey.html"));
+    });
+//default catch all route that leads to home
+  app.use(function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/home.html"));
+  });
+};
